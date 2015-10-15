94b969b27d8ffcbca100566c7c46bf9728cb700b
Gang.Wang <gang.wang.china@gmail.com>
2015-10-15 16:02:28 +0800

    为mtnet写demo

--- MTNetDemo/MTNetDemo/ViewController.m
+++ MTNetDemo/MTNetDemo/ViewController.m
@@ -8,7 +8,14 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#import "MTDownloaderHelper.h"
+#import "MTDownloadObjectModel.h"
+#import "MTDownloadRequest.h"
+#import "MTDownloadRecvData.h"
+
+@interface ViewController ()<MTDownloadRequestDelegate>
+
+@property (nonatomic, strong) MTDownloaderHelper * downloadHelper;
 
 @end
 
@@ -17,11 +24,58 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.downloadHelper = [[MTDownloaderHelper alloc] init];
+    [self.downloadHelper setTimeout:20];
+    [self.downloadHelper setMaxConcurrentTaskCount:5];
+    
+    
+    MTDownloadRequest * request = [[MTDownloadRequest alloc] init];
+    MTDownloadObjectModel * downloadMdel = [[MTDownloadObjectModel alloc] init];
+    downloadMdel.url = @"http://www.baidu.com";
+    request.downloadModel = downloadMdel;
+    
+    request.delegate  = self;
+    
+    [self.downloadHelper addDownloadRequest:request];
+    
+    
+    // cancel download
+    [self.downloadHelper cancelDownloadRequestByName:request.name];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+#pragma mark - MTDownloadRequestDelegate
+
+- (void) didTaskReceiveDataFinish   : (NSData *) theData    request:(MTDownloadRequest*) request
+{
+    NSLog(@"download ok");
+}
+
+- (void) taskDataDownloadFailed     : (NSError *) error     request:(MTDownloadRequest*) request
+{
+    NSLog(@"download failed:%@", error);
+}
+
+- (void) taskDataDownloadAtPercent  : (NSNumber *) aPercent request:(MTDownloadRequest*) request
+{
+    
+}
+
+- (void) taskDataPostAtPercent      : (NSNumber *) aPercent request:(MTDownloadRequest *) request
+{
+    
+}
+
+- (void) didTaskReceiveFilename     : (NSString *) aName    request:(MTDownloadRequest*) request
+{
+    
 }
 
 @end
